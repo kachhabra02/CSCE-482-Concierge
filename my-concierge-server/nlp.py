919aef4dee14fe3f8cb85e5519992214aa77225c
Will-K-T <21willdog42@gmail.com>
2024-02-26 09:58:40 -0600

    Initial testing of nltk and learning how it works.

--- my-concierge-server/nlp.py
+++ my-concierge-server/nlp.py
@@ -1,2 +1,15 @@
 import nltk
 
+# Download everything that nltk will need
+nltk.download('all')
+
+# Data to be used for nlp analysis
+example_sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
+
+# Obtain the tokens from the input data
+tokens = nltk.word_tokenize(example_sentence)
+
+# Finds the different parts of speech from the tokenized text
+tagged = nltk.pos_tag(tokens)
+
+print(tagged)
