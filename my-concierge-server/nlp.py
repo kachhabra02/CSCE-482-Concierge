87fc0e9c4c212d55aa973b2c09869d8c646cc9c7
Will-K-T <21willdog42@gmail.com>
2024-02-26 10:33:37 -0600

    Trying to figure out why the nltk.download() isn't finishing.

--- my-concierge-server/nlp.py
+++ my-concierge-server/nlp.py
@@ -1,7 +1,7 @@
 import nltk
 
 # Download everything that nltk will need
-nltk.download('all')
+nltk.download('punkt')
 
 # Data to be used for nlp analysis
 example_sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
