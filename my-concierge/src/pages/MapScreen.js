64d75d6b4756e784df342af539654e5ba0576f8f
Aubree Rodriguez <aubreerodri@DESKTOP-6IUIFJJ.localdomain>
2024-03-06 03:46:44 -0600

    assigning top ranked item to a specific pic and the other results are another image

--- my-concierge/src/pages/MapScreen.js
+++ my-concierge/src/pages/MapScreen.js
@@ -1,25 +1,45 @@
 import React from 'react';
-// import {GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+ import {GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 
-const center = {lat: 30.6212316, lng: -96.3403778}
+const center = {lat: 30.6212316, lng: -96.3403778};
 
 function MapScreen() {
-  return (        
-    <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-      <Map center={center} zoom={18} style={{width: '1000px', height: '500px'}}>
-        <Marker latitude={center.lat} longitude={center.lng}></Marker>
-      </Map>
-    </APIProvider>
-  )
+  //pass in list from backend
+  const renderList = [{ position: { lat: 30.6212316, lng: -96.3403778 }, rank: 1 },
+                      { position: { lat: 30, lng: -96 }, rank: 2 },
+                     ];
+  // Sort the list by rank
+  renderList.sort((a, b) => a.rank - b.rank);
 
-  // return (        
-  //   <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-  //     <GoogleMap center={center} zoom={18} mapContainerStyle={{width: '1000px', height: '500px'}}>
-  //       <></>
-  //     </GoogleMap>
-  //   </LoadScript>
-  // )
+  // Assign images based on rank
+  renderList.forEach((item, index) => {
+    if (index === 0) {
+      // Assign specific picture to rank 1
+      item.image = "/Images/goldPin.png";
+    } else {
+      // Assign another image for other ranks
+      item.image = "/Images/orangePin.png";
+    }
+  });
+  
+  const renderMarkers = () => {
+    const markers = [];
+    for (let i = 0; i < renderList.length; ++i) {
+      markers.push(<MarkerF position={renderList[i].position} icon={{ url: renderList[i].icon }}></MarkerF>);
+    }
+
+    return markers;
+  }
+  
+  return (     
+    <div>FX 
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap center={center} zoom={18} mapContainerStyle={{width: '1000px', height: '500px'}}>
+          {renderMarkers()}
+      </GoogleMap>
+    </LoadScript>
+    </div>
+  )
 }
 
 export default MapScreen
