63fe7b3397edbf40b522047882a7a9bb812ca015
ravishshardha <ravishshardha26@gmail.com>
2024-03-16 13:40:14 -0500

    added fav button

--- my-concierge/src/components/RestCard.jsx
+++ my-concierge/src/components/RestCard.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Star from "./Star";
 import ListGroup from "react-bootstrap/ListGroup";
 import Accordion from "react-bootstrap/Accordion";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/RestCardCss.css";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 function RestCard({ name, stars, reviews, cusines, address }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const onFavoriteClick = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src="holder.js/100px180" />
@@ -15,7 +22,12 @@ function RestCard({ name, stars, reviews, cusines, address }) {
           <Card.Title>{name}</Card.Title>
           <Star stars={stars} reviews={reviews} />
         </div>
-        <Card.Text>{cusines}</Card.Text>
+        <div className="d-flex flex-row justify-content-between w-100">
+          <Card.Text>{cusines}</Card.Text>
+          <button type="button" className="favorite-btn btn" onClick={onFavoriteClick}>
+            {isFavorite ? <FaHeart style={{ color: "#fb2323" }} /> : <FaRegHeart style={{ color: "#fb2323" }} />}
+          </button>
+        </div>
       </Card.Body>
       <ListGroup className="list-group-flush">
         <ListGroup.Item>Address: {address}</ListGroup.Item>
