82967060a5f262026e96dbd0c89711459faa94fe
ravishshardha <ravishshardha26@gmail.com>
2024-03-29 11:50:49 -0500

    added button for auto scroll

--- my-concierge/src/pages/CardScreen.js
+++ my-concierge/src/pages/CardScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Carousel from 'react-multi-carousel';
 import RestCard from '../components/RestCard';
 import ShoppingCart from "../components/ShoppingCart";
@@ -10,49 +10,130 @@ function CardScreen() {
   const [favItems, setFavItems] = useState([]);
   const [showShoppingCart, setShowShoppingCart] = useState(false);
 
-  const businessData = {
-    "business_id": "tnhfDv5Il8EaGSXZGiuQGg",
-    "name": "Garaje",
-    "address": "475 3rd St",
-    "city": "San Francisco",
-    "state": "CA",
-    "postal_code": "94107",
-    "latitude": 37.7817529521,
-    "longitude": -122.39612197,
-    "stars": 4.5,
-    "review_count": 1198,
-    "is_open": 1,
-    "attributes": {
-      "RestaurantsTakeOut": true,
-      "BusinessParking": {
-        "garage": false,
-        "street": true,
-        "validated": false,
-        "lot": false,
-        "valet": false
+  const carouselRef = useRef(null); // Reference for the carousel component
+
+  const businessData = [
+    {
+      "business_id": "tnhfDv5Il8EaGSXZGiuQGg",
+      "name": "Garaje",
+      "address": "475 3rd St",
+      "city": "San Francisco",
+      "state": "CA",
+      "postal_code": "94107",
+      "latitude": 37.7817529521,
+      "longitude": -122.39612197,
+      "stars": 4.5,
+      "review_count": 1198,
+      "is_open": 1,
+      "attributes": {
+        "RestaurantsTakeOut": true,
+        "BusinessParking": {
+          "garage": false,
+          "street": true,
+          "validated": false,
+          "lot": false,
+          "valet": false
+        }
+      },
+      "categories": [
+        "Mexican",
+        "Burgers",
+        "Gastropubs",
+        "Vegan",
+        "Other"
+      ],
+      "hours": {
+        "Monday": "10:00-21:00",
+        "Tuesday": "10:00-21:00",
+        "Friday": "10:00-21:00",
+        "Wednesday": "10:00-21:00",
+        "Thursday": "10:00-21:00",
+        "Sunday": "11:00-18:00",
+        "Saturday": "10:00-21:00"
       }
     },
-    "categories": [
-      "Mexican",
-      "Burgers",
-      "Gastropubs",
-      "Vegan",
-      "Other"
-    ],
-    "hours": {
-      "Monday": "10:00-21:00",
-      "Tuesday": "10:00-21:00",
-      "Friday": "10:00-21:00",
-      "Wednesday": "10:00-21:00",
-      "Thursday": "10:00-21:00",
-      "Sunday": "11:00-18:00",
-      "Saturday": "10:00-21:00"
+    {
+      "business_id": "tnhfDv5Il8EaGSXZGiuQGg",
+      "name": "Garaje2",
+      "address": "475 3rd St",
+      "city": "San Francisco",
+      "state": "CA",
+      "postal_code": "94107",
+      "latitude": 37.7817529521,
+      "longitude": -122.39612197,
+      "stars": 4.5,
+      "review_count": 1198,
+      "is_open": 1,
+      "attributes": {
+        "RestaurantsTakeOut": true,
+        "BusinessParking": {
+          "garage": false,
+          "street": true,
+          "validated": false,
+          "lot": false,
+          "valet": false
+        }
+      },
+      "categories": [
+        "Mexican",
+        "Burgers",
+        "Gastropubs",
+        "Vegan",
+        "Other"
+      ],
+      "hours": {
+        "Monday": "10:00-21:00",
+        "Tuesday": "10:00-21:00",
+        "Friday": "10:00-21:00",
+        "Wednesday": "10:00-21:00",
+        "Thursday": "10:00-21:00",
+        "Sunday": "11:00-18:00",
+        "Saturday": "10:00-21:00"
+      }
+    },
+    {
+      "business_id": "tnhfDv5Il8EaGSXZGiuQGg",
+      "name": "Garaje3",
+      "address": "475 3rd St",
+      "city": "San Francisco",
+      "state": "CA",
+      "postal_code": "94107",
+      "latitude": 37.7817529521,
+      "longitude": -122.39612197,
+      "stars": 4.5,
+      "review_count": 1198,
+      "is_open": 1,
+      "attributes": {
+        "RestaurantsTakeOut": true,
+        "BusinessParking": {
+          "garage": false,
+          "street": true,
+          "validated": false,
+          "lot": false,
+          "valet": false
+        }
+      },
+      "categories": [
+        "Mexican",
+        "Burgers",
+        "Gastropubs",
+        "Vegan",
+        "Other"
+      ],
+      "hours": {
+        "Monday": "10:00-21:00",
+        "Tuesday": "10:00-21:00",
+        "Friday": "10:00-21:00",
+        "Wednesday": "10:00-21:00",
+        "Thursday": "10:00-21:00",
+        "Sunday": "11:00-18:00",
+        "Saturday": "10:00-21:00"
+      }
     }
-  };
+];
 
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -71,7 +152,7 @@ function CardScreen() {
   };
 
   useEffect(() => console.log(favItems), [favItems])
-  const address = businessData.address + ", " + businessData.city + ", " + businessData.state + ", " + businessData.postal_code ;
+  const address = businessData[0].address + ", " + businessData[0].city + ", " + businessData[0].state + ", " + businessData[0].postal_code ;
 
   const renderButtonGroupOutside = ({ totalItems, currentSlide, ...props }) => (
     <ul className="custom-dots">
@@ -82,6 +163,24 @@ function CardScreen() {
       ))}
     </ul>
   );
+
+  // Function to handle scrolling to a specific slide
+  const scrollToSlide = (index) => {
+    carouselRef.current.goToSlide(index);
+  };
+
+  // Function to find the index of an item by its name
+  const findItemIndexByName = (name) => {
+    return businessData.findIndex(business => business.name === name);
+  };
+
+  // Click event handler for buttons
+  const handleButtonClick = (name) => {
+    const index = findItemIndexByName(name);
+    if (index !== -1) {
+      scrollToSlide(index);
+    }
+  };
 
   return (
     <div>
@@ -101,15 +200,25 @@ function CardScreen() {
       </div>
 
       <h1>Items:</h1>
-        <Carousel responsive={responsive} showDots={true} renderDotsOutside={renderButtonGroupOutside} removeArrowOnDeviceType={["tablet", "mobile"]}>
-          {/*Change the stars and reviews to json data*/}
-          <div><RestCard name={businessData.name} stars={businessData.stars} reviews={businessData.review_count >= 1000 ? "1k+" : businessData.review_count} cusines={businessData.categories} address={address} favItems={favItems} setFavItems={setFavItems}/></div>
-          <div><RestCard name="exa" stars={businessData.stars} reviews={businessData.review_count >= 1000 ? "1k+" : businessData.review_count} cusines={businessData.categories} address={address} favItems={favItems} setFavItems={setFavItems}/></div>
-          <div><RestCard name="ex3" stars={businessData.stars} reviews={businessData.review_count >= 1000 ? "1k+" : businessData.review_count} cusines={businessData.categories} address={address} favItems={favItems} setFavItems={setFavItems}/></div>
-          <div>Item 4</div>
-        </Carousel>
 
-        {showShoppingCart && (
+      {businessData.map((business, index) => (
+        <button key={index} onClick={() => handleButtonClick(business.name)}>{business.name}</button>
+      ))}
+        
+      <Carousel 
+        ref={carouselRef} // Set the ref for the carousel
+        responsive={responsive} 
+        showDots={true} 
+        renderDotsOutside={renderButtonGroupOutside} 
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+      >
+        {businessData.map((business, index) => (
+          <div key={index}><RestCard name={business.name} stars={business.stars} reviews={business.review_count >= 1000 ? "1k+" : business.review_count} cusines={business.categories} address={address} favItems={favItems} setFavItems={setFavItems}/></div>
+        ))}
+        <div>Item 4</div>
+      </Carousel>
+
+      {showShoppingCart && (
         <ShoppingCart
           favItems={favItems}
           setFavItems={setFavItems}
@@ -120,4 +229,4 @@ function CardScreen() {
   )
 }
 
-export default CardScreen
+export default CardScreen;
