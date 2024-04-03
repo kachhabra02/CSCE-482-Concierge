a3ec5872f25fff5e2af259f5b02a20e6015c32ad
Arpan Kumar <arpsku17@gmail.com>
2024-04-03 11:24:22 -0500

    Wrote all endpoints and database scripts

--- my-concierge-server/app.py
+++ my-concierge-server/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import database
+import random
 
 app = Flask(__name__)
 
@@ -7,6 +8,37 @@ app = Flask(__name__)
 def home():
     restaurants = database.get_restaurants()
     return render_template('home.html', restaurants=restaurants)
+
+@app.route('/api/resource/prompt', methods=['GET'])
+def prompt():
+    prompt = request.args.get('prompt')
+    original_user_preference_vector_str = request.args.get('original_user_preference_vector')
+    original_user_preference_vector = [int(x) for x in original_user_preference_vector_str.strip('[]').split(',')]
+
+    # final_user_preference_vector = getUserPreferenceVector(prompt, original_user_preference_vector) // Incorporate Will's Code
+    
+    final_user_preference_vector = [random.randint(0, 5) for _ in range(32)]
+    data = {
+        'prompt': prompt,
+        'original_user_preference_vector': original_user_preference_vector,
+        'final_user_preference_vector': final_user_preference_vector,
+    }
+    return jsonify(data)
+
+@app.route('/api/resource/reccomendation', methods=['GET'])
+def reccomendation():
+    location = request.args.get('location')
+    user_preference_vector_str = request.args.get('user_preference_vector')
+    user_preference_vector = [int(x) for x in user_preference_vector_str.strip('[]').split(',')]    
+    # return getReccomendation(location, user_preference_vector) // Incorporate Krish's Code
+    
+    # delete code below, only for testing purposes
+    user_preference_vector = [random.randint(0, 5) for _ in range(32)]
+    data = {
+        'location': location,
+        'user_preference_vector': user_preference_vector,
+    }
+    return jsonify(data)
 
 @app.errorhandler(404)
 def page_not_found(e):
