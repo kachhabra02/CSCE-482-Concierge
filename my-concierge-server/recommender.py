10854531a1be5a373a1d4504a3069117e0c35192
Krish Chhabra <kachhabra02@tamu.edu>
2024-04-05 16:39:04 -0500

    Added basic layout for recommender file

---
+++ my-concierge-server/recommender.py
@@ -0,0 +1,38 @@
+# Imports For Support Functions
+# TODO
+
+# Imports For Important Function
+# TODO
+
+# Functions for temporary use only
+# Location -> List of Dictionaries
+def getRestaurants(location):
+    pass
+
+# Array of Categories Indexed by category_id
+def get_categories():
+    pass
+
+# Array of Attributes Indexed by category_id
+def get_attributes():
+    pass
+
+
+
+# Recommendations Function
+def getRecommendation(location, user_preference_vector):
+    # Get Restaurants in City
+    restaurants = getRestaurants(location)
+
+    # Construct Restaurant Category Matrix
+        # Convert Restaurant Category Sum Into Array of {0,1}
+        # Merge Arrays Into Matrix in Order of Restaurant List
+    
+    # Optional: Update UPV based on category relationships
+
+    # Perform Product to Get Restaurant Scores
+
+    # Order Restaurants by Score (desc)
+
+
+    pass
