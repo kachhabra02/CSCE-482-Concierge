8f2ca9da0098af4ea9ebef594ee0507d82cb4372
danielamtzb <daniela.mtzbanda@gmail.com>
2024-04-08 18:44:19 -0500

    update on the chatbox

--- my-concierge/src/components/ChatBox.jsx
+++ my-concierge/src/components/ChatBox.jsx
@@ -1,23 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Form } from 'react-bootstrap';
 import '../css/ChatBox.css'; // Import the CSS file
 import robotImage from '../img/robot.png';
 
 const ChatBox = () => {
   const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState('');
+  const [userMessage, setUserMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [botMessage, setBotMessage] = useState('');
+  const [selectedCity, setSelectedCity] = useState(''); //user selected city
 
-  const handleSendMessage = () => {
-    // Add the new message to the messages array
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { id: prevMessages.length + 1, text: newMessage },
+
   
-    ]);
+  const handleSendMessage = async () => {
+    // Add the new message to the messages array
+    setMessages([...messages, { text: userMessage, sender: 'user' }]);
+
+    if(!selectedCity){
+      alert('Hello! To have the best user experience please select a city before continuing.')
+      return;
+    }
+
+    //Generate response for user based on input
+    setIsLoading(true);
+    const response = await generateBotResponse(userMessage, selectedCity);
+    setBotMessage(response);
+    setMessages([...messages, {text: response, sender: 'bot'}]);
+    setIsLoading(false);
 
     // Clear the input field
-    setNewMessage('');
+    setUserMessage('');
   };
+
+  //Contact API response
+  const generateBotResponse = (message, city) => {
+    if(!messages){ // Initial conversation
+      return city;
+    } else{
+      return message;
+    }
+  }
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
@@ -26,35 +48,61 @@ const ChatBox = () => {
     }
   };
 
+  useEffect(() => {
+    const greetingMessage = (
+      <div className="greetings">
+        Hi there! I am your virtual concierge here to help you find the best place to dine in.
+        Please choose a city to get started:
+        <br />
+        <button onClick={() => setSelectedCity('Philadelphia')}>Philadelphia</button>
+        <button onClick={() => setSelectedCity('Tuscon')}>Tucson</button>
+        <button onClick={() => setSelectedCity('Reno')}> Reno </button>
+        <button onClick={() => setSelectedCity('New Orleans')}>New Orleans</button>
+        <button onClick={() => setSelectedCity('Tampa')}> Tampa </button>
+        <button onClick={() => setSelectedCity('Nashville')}>Nashville</button>
+      </div>
+    );
+    setBotMessage(greetingMessage);
+  }, []);
+
   return (
     <Container className = "chatbox-container">
       <Row>
-        <Col className="col-4" >
+        <Col>
           {/* Image div */}
           <div className="image-container">
             <img src={robotImage} alt="Robot" />
           </div>
         </Col>
-          <Col className='messages-container col-8' >
-            {/* Display messages .reverse()*/}
+        <Col>
+        {botMessage}
+          <Col className='messages-container' >
+            {/* Display messages*/}
             {messages.slice().map((message) => (
               <div key={message.id} className="message-container">
                 {message.text}
               </div>
             ))}
           </Col>
-          <Col className = "input-container col-8">
+          <Col className = "input-container">
             {/* Input form */}
-            <Form.Group controlId="formMessage">
-              <Form.Control
-                type="text"
-                placeholder="Type your message..."
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-                onKeyPress={handleKeyPress}
-              />
-            </Form.Group>
+            {isLoading ? (
+              <div className="loading-container">
+                <span>thinking...</span>
+              </div>
+            ) : (
+              <Form.Group controlId="formMessage">
+                <Form.Control
+                  type="text"
+                  placeholder="Type your message..."
+                  value={userMessage}
+                  onChange={(e) => setUserMessage(e.target.value)}
+                  onKeyPress={handleKeyPress}
+                />
+              </Form.Group>
+            )}
           </Col>
+        </Col>
       </Row>
     </Container>
   );
