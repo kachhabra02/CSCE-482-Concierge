b07551a6fb0d2b65b752a145b21be09f9b54c3fb
Arpan Kumar <arpsku17@gmail.com>
2024-04-14 22:10:56 -0500

    added pictures to most chain restaurants

--- my-concierge-server/recommender.py
+++ my-concierge-server/recommender.py
@@ -11,23 +11,23 @@ def getRecommendations(location, user_preference_vector):
 
     # Get Categories and Attributes
     categories = database.getCategories()
-    print(categories)
+    # print(categories)
     num_cat = len(categories)
     attributes = database.getAttributes()
-    print(attributes)
+    # print(attributes)
     num_att = len(attributes)
 
     # Construct Restaurant Category Matrix
     cat_arrs = [[1 if digit == '1' else 0 for digit in bin(restaurant['category_sum'])[2:].zfill(num_cat)] for restaurant in restaurants]
     cat_mat = np.array(cat_arrs)
-    print(cat_mat.shape)
+    # print(cat_mat.shape)
 
     
     # Optional: Update UPV based on category relationships
 
     # Perform Product to Get Restaurant Scores
     user_preference_vector = np.array(user_preference_vector[::-1])
-    print(user_preference_vector.shape)
+    # print(user_preference_vector.shape)
     scores = np.dot(cat_mat, user_preference_vector)
 
     # Order Restaurants by Score (Descending)
@@ -43,16 +43,24 @@ def getRecommendations(location, user_preference_vector):
         recommendation = restaurants[order[rank]]
         recommendation['rank'] = rank
 
+        if recommendation['num_images'] <= 0:
+            for restaurant in restaurants:
+                if restaurant['name'] == recommendation['name'] and restaurant['num_images'] > 0:
+                    recommendation['base_image_url'] = restaurant['base_image_url']
+                    recommendation['num_images'] = restaurant['num_images']
+
+        # print(recommendation['num_images'])
+
         # Extract Categories
-        print(recommendation['category_sum'])
+        # print(recommendation['category_sum'])
         recommendation['categories'] = [categories[i] for i, digit in enumerate(bin(recommendation['category_sum'])[2:].zfill(num_cat)) if digit == '1']
-        print(recommendation['categories'])
+        # print(recommendation['categories'])
         del recommendation['category_sum']
 
         # Extract Categories
-        print(recommendation['attribute_sum'])
+        # print(recommendation['attribute_sum'])
         recommendation['attributes'] = [attributes[i] for i, digit in enumerate(bin(recommendation['attribute_sum'])[2:].zfill(num_att)) if digit == '1']
-        print(recommendation['attributes'])
+        # print(recommendation['attributes'])e
         del recommendation['attribute_sum']
 
         recommendations.append(recommendation)
@@ -61,4 +69,4 @@ def getRecommendations(location, user_preference_vector):
 
 if __name__ == '__main__':
     recs = getRecommendations('Tampa', [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    print(recs)
+    # print(recs)
